3b3734ec6231687e8dc5ddb1f4878d49c241323c
Josue Vega <j.vegav@uniandes.edu.co>
2024-05-25 12:56:29 -0500

    commit ocon variables de entorno

--- latencia/latencia/src/main/java/com/latencia/latencia/service/MessageVerifyService.java
+++ latencia/latencia/src/main/java/com/latencia/latencia/service/MessageVerifyService.java
@@ -3,10 +3,9 @@ import com.twilio.Twilio;
 import com.twilio.rest.verify.v2.service.VerificationCheck;
 
 public class MessageVerifyService {
-    // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "AC1ecafd15aa3fc0a2974c61a972f1453f";
-    public static final String AUTH_TOKEN = "a469e544dcf4c2a3ef71c1d33567dd2c";
-    public static final String SERVICE_AUTH = "VA5af1ad8306482f8c1bf2d501647344f9";
+  private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+  private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+  private static final String SERVICE_AUTH = System.getenv("TWILIO_SERVICE_AUTH");
 
 
   public String verificar(String numero, String telefono) {
