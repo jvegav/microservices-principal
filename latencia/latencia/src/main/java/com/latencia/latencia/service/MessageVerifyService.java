d04d9b64ce0ca8a67e4068f567e32847af259188
Josue Vega <j.vegav@uniandes.edu.co>
2024-05-25 12:33:11 -0500

    nuevos apis

--- latencia/latencia/src/main/java/com/latencia/latencia/service/MessageVerifyService.java
+++ latencia/latencia/src/main/java/com/latencia/latencia/service/MessageVerifyService.java
@@ -5,7 +5,7 @@ import com.twilio.rest.verify.v2.service.VerificationCheck;
 public class MessageVerifyService {
     // Find your Account Sid and Token at twilio.com/console
     public static final String ACCOUNT_SID = "AC1ecafd15aa3fc0a2974c61a972f1453f";
-    public static final String AUTH_TOKEN = "f09f9d91cd52786bd7a4ce96b421dae2";
+    public static final String AUTH_TOKEN = "a469e544dcf4c2a3ef71c1d33567dd2c";
     public static final String SERVICE_AUTH = "VA5af1ad8306482f8c1bf2d501647344f9";
 
 
--- notification-service/src/main/java/com/latencia/notification_service/PublishOtp.java
+++ notification-service/src/main/java/com/latencia/notification_service/PublishOtp.java
@@ -4,7 +4,7 @@ import com.twilio.rest.verify.v2.service.Verification;
 
 public class PublishOtp {
     public static final String ACCOUNT_SID = "AC1ecafd15aa3fc0a2974c61a972f1453f";
-    public static final String AUTH_TOKEN = "f09f9d91cd52786bd7a4ce96b421dae2";
+    public static final String AUTH_TOKEN = "a469e544dcf4c2a3ef71c1d33567dd2c";
     public static final String SERVICE_AUTH = "VA5af1ad8306482f8c1bf2d501647344f9";
 
     public void publish(String number) {
